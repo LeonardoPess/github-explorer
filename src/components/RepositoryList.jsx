6bb0a23113f22e3b922c373528b68104872d3980
Leonardo Pessoa <leonardopessoa799@gmail.com>
2021-12-03 01:59:50 -0300

    first component 'RespositoryList'

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,45 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
